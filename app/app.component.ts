0c4497b245a9fdffbfcc9cb69a492a269971843c
yashish <yashishraj@gmail.com>
2016-12-01 21:10:50 -0500

    Added User class and moved template to file

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,19 +1,26 @@
 import { Component } from "@angular/core";
+import {User} from "./shared/user/user";
 
 @Component({
   selector: "my-app",
-  template: `
-    <StackLayout>
-      <Image src="res://logo_login" stretch="none" horizontalAlignment="center"></Image>
-      <TextField hint="Email Address" keyboardType="email"
-        autocorrect="false" autocapitalizationType="none">
-      </TextField>
-      <TextField hint="Password" secure="true"></TextField>
-
-      <Button text="Sign in" class="submit-button"></Button>
-      <Button text="Register"></Button>
-    </StackLayout>
-  `,
+  templateUrl: "pages/login/login.html",
   styleUrls: ["pages/login/login-common.css", "pages/login/login.css"]
 })
-export class AppComponent {}
+
+export class AppComponent {
+  user: User;
+  email = "nativescriptrocks@telerik.com";
+  isLoggingIn = true;
+
+  constructor(){
+    this.user = new User();
+  }
+
+  submit(){
+    alert("Hello from Email:" + this.user.email);
+  }
+
+  toggleDisplay(){
+    this.isLoggingIn = !this.isLoggingIn;
+  }
+}
